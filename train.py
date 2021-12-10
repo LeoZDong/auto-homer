e2afd0f723d29b07f13f95ebd2222b236d893044
Leo Dong <leodong0315@gmail.com>
2021-12-10 03:07:11 -0800

    change setup

--- train.py
+++ train.py
@@ -22,21 +22,21 @@ args = parser.parse_args()
 # Generate initial output
 generate.generate('gen_init.txt', model_dir=None)
 
-# config = AutoConfig.from_pretrained('gpt2')
-# model = AutoModelForCausalLM.from_config(config)
+config = AutoConfig.from_pretrained('gpt2')
+model = AutoModelForCausalLM.from_config(config)
 
-# training_args = TrainingArguments("test_trainer")
-# training_args.num_train_epochs = args.n_epochs
-# training_args.learning_rate = args.lr
-# training_args.batch_size = args.batch_size
-# training_args.save_steps = args.save_steps
+training_args = TrainingArguments("test_trainer")
+training_args.num_train_epochs = args.n_epochs
+training_args.learning_rate = args.lr
+training_args.batch_size = args.batch_size
+training_args.save_steps = args.save_steps
 
-# train_dataset = data.get_dataset()
+train_dataset = data.get_dataset()
 
-# trainer = Trainer(model=model,
-#                   args=training_args,
-#                   train_dataset=train_dataset,
-#                   eval_dataset=train_dataset)
-# trainer.train()
-# model.save_pretrained(save_directory='models/gpt2_homer')
-# generate.generate('gen_final.txt', model_dir='models/gpt2_homer')
+trainer = Trainer(model=model,
+                  args=training_args,
+                  train_dataset=train_dataset,
+                  eval_dataset=train_dataset)
+trainer.train()
+model.save_pretrained(save_directory='models/gpt2_homer')
+generate.generate('gen_final.txt', model_dir='models/gpt2_homer')
